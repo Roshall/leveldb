deb2677d2da2ccc683e388d5d1ab0925e0308047
Roshall <15504658671@163.com>
2022-04-14 12:18:28 +0800

    change namespace to co-exist with leveldb

--- port/port.h
+++ port/port.h
@@ -2,18 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#ifndef STORAGE_LEVELDB_PORT_PORT_H_
-#define STORAGE_LEVELDB_PORT_PORT_H_
+#ifndef STORAGE_LEVELDB_HOT_PORT_PORT_H_
+#define STORAGE_LEVELDB_HOT_PORT_PORT_H_
 
 #include <string.h>
 
 // Include the appropriate platform specific file below.  If you are
 // porting to a new platform, see "port_example.h" for documentation
 // of what the new port_<platform>.h file must provide.
-#if defined(LEVELDB_PLATFORM_POSIX) || defined(LEVELDB_PLATFORM_WINDOWS)
+#if defined(LEVELDB_HOT_PLATFORM_POSIX) || defined(LEVELDB_HOT_PLATFORM_WINDOWS)
 #include "port/port_stdcxx.h"
-#elif defined(LEVELDB_PLATFORM_CHROMIUM)
+#elif defined(LEVELDB_HOT_PLATFORM_CHROMIUM)
 #include "port/port_chromium.h"
 #endif
 
-#endif  // STORAGE_LEVELDB_PORT_PORT_H_
+#endif  // STORAGE_LEVELDB_HOT_PORT_PORT_H_
